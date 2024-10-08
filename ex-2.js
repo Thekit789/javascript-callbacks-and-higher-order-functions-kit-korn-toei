1cfd11a005f728aac63bb530e72825283cd711fe
HaruDev <sarankhtn@gmail.com>
2024-10-08 15:13:20 +0700

    feat : add all the assignments

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,23 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      count++;
+    }
+  }
+  return operation(count);
+}
+
+function generateCheckQuilty(roomNumber) {
+  return function(count) {
+    if (count >= 5) {
+      return (`นักเรียนห้องที่ ${roomNumber} ผ่านเกณฑ์ ✅`);
+    } else {
+      return (`นักเรียนห้องที่ ${roomNumber} ไม่ผ่านเกณฑ์ ❌`);
+    }
+  };
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +26,9 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
-
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, generateCheckQuilty(1));
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, generateCheckQuilty(2));
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, generateCheckQuilty(3));
+console.log(scoreRoom1Result)
+console.log(scoreRoom2Result)
+console.log(scoreRoom3Result)
