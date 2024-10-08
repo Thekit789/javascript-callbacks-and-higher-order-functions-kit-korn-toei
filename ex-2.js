cb1b4199ac23f3b039a8e57a143c4e69ddb83c5c
Kittisak <kittisaksa@gmail.com>
2024-10-08 14:54:28 +0700

    add: all assignment cb & hof ex1-ex2

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,13 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (const i in array) {
+    if (array[i] > 70) {
+      count++;
+    }
+  }
+  operation(count);
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +17,38 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let resultRoom1;
+let resultRoom2;
+let resultRoom3;
+
+atLeastFive(studentScoresRoom1, function (operation) {
+  if (operation >= 5) {
+    resultRoom1 = "นักเรียนห้องที่ 1 ผ่านเกณฑ์ ✅";
+  } else {
+    resultRoom1 = "นักเรียนห้องที่ 1 ไม่ผ่านเกณฑ์ ❌";
+  }
+});
+
+atLeastFive(studentScoresRoom2, function (operation) {
+  if (operation >= 5) {
+    resultRoom2 = "นักเรียนห้องที่ 2 ผ่านเกณฑ์ ✅";
+  } else {
+    resultRoom2 = "นักเรียนห้องที่ 2 ไม่ผ่านเกณฑ์ ❌";
+  }
+});
+
+atLeastFive(studentScoresRoom3, function (operation) {
+  if (operation >= 5) {
+    resultRoom3 = "นักเรียนห้องที่ 3 ผ่านเกณฑ์ ✅";
+  } else {
+    resultRoom3 = "นักเรียนห้องที่ 3 ไม่ผ่านเกณฑ์ ❌";
+  }
+});
+
+let scoreRoom1Result = resultRoom1;
+let scoreRoom2Result = resultRoom2;
+let scoreRoom3Result = resultRoom3;
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
